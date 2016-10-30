2be7c5feecdda0be26a1288f61fe69b6dbf816b0
George Dewar <george@dewar.co.nz>
2016-10-30 22:12:09 +1300

    Start adding Taskman options

--- lib/taskman.rb
+++ lib/taskman.rb
@@ -1,5 +1,33 @@
 module Taskman
+  DEFAULT_OPTIONS = {
+      heartbeat_stop_timeout: 10,
+      heartbeat_free_timeout: 15,
+      redis_url: "redis://127.0.0.1:6379"
+  }
+  @options = DEFAULT_OPTIONS
 
+  # A task will terminate (in preparation for another worker claiming it) if it fails to send heartbeats for this
+  # duration
+  def self.heartbeat_stop_timeout=(value)
+    @options[:heartbeat_stop_timeout] = value
+  end
+
+  # A task will become available for other workers if no heartbeat is received for this duration
+  def self.heartbeat_free_timeout=(value)
+    @options[:heartbeat_free_timeout] = value
+  end
+
+  def self.redis_url=(value)
+    @options[:redis_url] = value
+  end
+
+  def self.setup
+    yield self
+  end
+
+  def self.options
+    DEFAULT_OPTIONS.merge(@options)
+  end
 end
 
 require 'taskman/task'
--- test/test_taskman.rb
+++ test/test_taskman.rb
@@ -14,7 +14,14 @@ class TaskmanTest < Minitest::Test
     task_options autorun: false
   end
 
-  def test_options
+  def test_taskman_options
+    Taskman.setup do |config|
+      config.heartbeat_stop_timeout = 11
+    end
+    assert_equal 11, Taskman.options[:heartbeat_stop_timeout]
+  end
+
+  def test_task_options
     assert_equal TestTask.get_task_options, {
         autorun: true,
         repeat: :run_once
